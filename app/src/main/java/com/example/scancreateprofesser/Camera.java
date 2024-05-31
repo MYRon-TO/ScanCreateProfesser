2a0be2d6c8a0e6b14dd1927698ed767a0039f943
see <1611215188@qq.com>
2024-05-31 14:37:42 +0800

    基本完成camera页面，可以完成基本功能后微调. 添加相机权限和相关依赖项，可以进行预览

---
+++ app/src/main/java/com/example/scancreateprofesser/Camera.java
@@ -0,0 +1,51 @@
+package com.example.scancreateprofesser;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.view.LifecycleCameraController;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Camera extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        PreviewView pv = findViewById(R.id.main_preview_view);
+
+        // 这部分代码是初始化CameraController并和控件PreviewView绑定
+        LifecycleCameraController cameraController =
+                new LifecycleCameraController(this);
+        cameraController.bindToLifecycle(this);
+        cameraController.setCameraSelector(CameraSelector.DEFAULT_BACK_CAMERA);
+        pv.setController(cameraController);
+    }
+}
